1d07cb9108b4ce3cd29fe9ffa3ba9e5aa82a0a14
Jeron <norejudeisa@gmail.com>
2023-06-26 11:12:50 +0000

    Remove bottom sheet background color

--- lib/widgets/health_needs.dart
+++ lib/widgets/health_needs.dart
@@ -18,97 +18,105 @@ class HealthNeeds extends StatelessWidget {
       CustomIcon(name: "Covid-19", icon: 'assets/virus.png'),
       CustomIcon(name: "Pharmacy", icon: 'assets/drug.png'),
     ];
-    List<CustomIcon> specializedCare = [
+    List<CustomIcon> specialisedCared = [
       CustomIcon(name: "Diabetes", icon: 'assets/blood.png'),
       CustomIcon(name: "Health Care", icon: 'assets/health_care.png'),
       CustomIcon(name: "Dental", icon: 'assets/tooth.png'),
       CustomIcon(name: "Insured", icon: 'assets/insurance.png'),
     ];
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: List.generate(customIcons.length, (index) {
         return Column(
           children: [
             InkWell(
-              borderRadius: BorderRadius.circular(90),
               onTap: () {
                 if (index == customIcons.length - 1) {
                   showModalBottomSheet(
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20),
-                      ),
-                    ),
-                    backgroundColor: Colors.white,
-                    showDragHandle: true,
                     context: context,
+                    showDragHandle: true,
                     builder: (context) {
                       return Container(
                         width: double.infinity,
-                        height: 350,
                         padding: const EdgeInsets.all(20),
+                        height: 350,
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text("Health Needs", style: Theme.of(context).textTheme.titleLarge),
-                            const SizedBox(height: 15),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: List.generate(healthNeeds.length, (index) {
-                                return Column(
-                                  children: [
-                                    InkWell(
-                                      borderRadius: BorderRadius.circular(90),
-                                      onTap: () {},
-                                      child: Container(
-                                        width: 60,
-                                        height: 60,
-                                        padding: const EdgeInsets.all(15),
-                                        decoration: BoxDecoration(
-                                          color: Theme.of(context).colorScheme.primaryContainer.withOpacity(0.4),
-                                          shape: BoxShape.circle,
-                                        ),
-                                        child: Image.asset(
-                                          healthNeeds[index].icon,
-                                        ),
-                                      ),
-                                    ),
-                                    const SizedBox(height: 6),
-                                    Text(healthNeeds[index].name)
-                                  ],
-                                );
-                              }),
+                            // HEALTH NEEDS SECTION
+                            Text(
+                              "Health Needs",
+                              style: Theme.of(context).textTheme.titleLarge,
                             ),
-                            const SizedBox(height: 30),
-                            Text("Specialised Care", style: Theme.of(context).textTheme.titleLarge),
                             const SizedBox(height: 15),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: List.generate(specializedCare.length, (index) {
-                                return Column(
-                                  children: [
-                                    InkWell(
-                                      borderRadius: BorderRadius.circular(90),
-                                      onTap: () {},
-                                      child: Container(
-                                        width: 60,
-                                        height: 60,
-                                        padding: const EdgeInsets.all(15),
-                                        decoration: BoxDecoration(
-                                          color: Theme.of(context).colorScheme.primaryContainer.withOpacity(0.4),
-                                          shape: BoxShape.circle,
-                                        ),
-                                        child: Image.asset(
-                                          specializedCare[index].icon,
+                              children: List.generate(
+                                healthNeeds.length,
+                                (index) {
+                                  return Column(
+                                    children: [
+                                      InkWell(
+                                        onTap: () {},
+                                        borderRadius: BorderRadius.circular(90),
+                                        child: Container(
+                                          width: 60,
+                                          height: 60,
+                                          padding: const EdgeInsets.all(15),
+                                          decoration: BoxDecoration(
+                                            color: Theme.of(context).colorScheme.primaryContainer.withOpacity(0.4),
+                                            shape: BoxShape.circle,
+                                          ),
+                                          child: Image.asset(
+                                            healthNeeds[index].icon,
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    const SizedBox(height: 6),
-                                    Text(specializedCare[index].name)
-                                  ],
-                                );
-                              }),
+                                      const SizedBox(height: 6),
+                                      Text(healthNeeds[index].name)
+                                    ],
+                                  );
+                                },
+                              ),
+                            ),
+                            SizedBox(height: 30),
+                            // SPECIALISED CARE SECTION
+
+                            Text(
+                              "Specialised Care",
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                            const SizedBox(height: 15),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: List.generate(
+                                specialisedCared.length,
+                                (index) {
+                                  return Column(
+                                    children: [
+                                      InkWell(
+                                        onTap: () {},
+                                        borderRadius: BorderRadius.circular(90),
+                                        child: Container(
+                                          width: 60,
+                                          height: 60,
+                                          padding: const EdgeInsets.all(15),
+                                          decoration: BoxDecoration(
+                                            color: Theme.of(context).colorScheme.primaryContainer.withOpacity(0.4),
+                                            shape: BoxShape.circle,
+                                          ),
+                                          child: Image.asset(
+                                            specialisedCared[index].icon,
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(height: 6),
+                                      Text(specialisedCared[index].name)
+                                    ],
+                                  );
+                                },
+                              ),
                             )
                           ],
                         ),
@@ -117,6 +125,7 @@ class HealthNeeds extends StatelessWidget {
                   );
                 }
               },
+              borderRadius: BorderRadius.circular(90),
               child: Container(
                 width: 60,
                 height: 60,
